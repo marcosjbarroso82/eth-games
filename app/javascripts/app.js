2bcb992075e227d24374c46708fa5d9a582f57e9
m <marcosjbarroso82@gmail.com>
2018-04-22 17:51:53 -0300

    refresh

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -27,11 +27,22 @@ new Vue({
   },
   components: { App },
   methods: {
+    refresh: function() {
+      var vm = this;
+      vm.getGamesCounter().then(function() {
+          vm.getClosedJoinGames().then(function() {
+            vm.getMygames().then(function(){
+              vm.getAvailableGames();
+            });
+          });
+      });
+    },
+
     getDebug: function() {console.log('debug');},
     getGamesCounter: function() {
       var vm = this;
       var meta;
-      MultiRPS.deployed().then(function(instance) {
+      return MultiRPS.deployed().then(function(instance) {
         var result = instance.gamesCounter.call({from: vm.account});
         return result;
       }).then(function(value) {
@@ -46,7 +57,7 @@ new Vue({
     getClosedJoinGames: function() {
       var vm = this;
       var meta;
-      MultiRPS.deployed().then(function(instance) {
+      return MultiRPS.deployed().then(function(instance) {
         var result = instance.getClosedJoinGames.call({from: vm.account});
         return result;
       }).then(function(value) {
@@ -69,59 +80,24 @@ new Vue({
       for (var i=0; i < vm.gamesCounter; i++) {
           games.push(i);
       }
-
-      // vm.availableGames = value.filter( ( el ) => !vm.myGames.includes( el ) );
-      // games = games.filter( ( el ) => !vm.closedJoinGames.includes( el ));
-// debugger;
-
-      // for(var i=0; i < games.length; i++) {
-      //   console.log(games[i], typeof(games[i]));
-      // }
-      games = games.filter( function( el ) {
-        console.log(el, vm.closedJoinGames.indexOf( el ), typeof(el));
-      return vm.closedJoinGames.indexOf( el ) < 0;
-    } );
-// debugger;
-
-    games = games.filter( function( el ) {
-      console.log(el, vm.myGames.indexOf( el ), typeof(el));
-    return vm.myGames.indexOf( el ) < 0;
-  } );
-
-
-      // games = games.filter( ( el ) => !vm.closedJoinGames.includes( el ) && vm.myGames.includes( el ));
+      games = games.filter( function( el ) { return vm.closedJoinGames.indexOf( el ) < 0;});
+      games = games.filter( function( el ) { return vm.myGames.indexOf( el ) < 0;});
       vm.availableGames = games;
-
-      // var vm = this;
-      // var meta;
-      // MultiRPS.deployed().then(function(instance) {
-      //   var result = instance.getAvailableGames.call({from: vm.account});
-      //   return result;
-      // }).then(function(value) {
-      //   // vm.availableGames = value;
-      //   vm.availableGames = value.filter( ( el ) => !vm.myGames.includes( el ) );
-      // }).catch(function(e) {
-      //   console.log(e);
-      // });
     },
     getMygames: function() {
       var vm = this;
       var meta;
-      MultiRPS.deployed().then(function(instance) {
+      return MultiRPS.deployed().then(function(instance) {
         console.log('first then');
         var result = instance.getPlayerGames.call(vm.account, {from: vm.account});
-        // debugger;
         return result;
       }).then(function(value) {
         console.log('second then');
         vm.myGames = [];
         for (var i=0; i < value.length; i++) {
             vm.myGames.push(value[i].toNumber());
-
         }
-        // vm.myGames = value;
       }).catch(function(e) {
-        // debugger;
         console.log(e);
       });
     },
@@ -134,13 +110,11 @@ new Vue({
           alert("There was an error fetching your accounts.");
           return;
         }
-
         if (accs.length == 0) {
           alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
           return;
         }
         vm.account = accs[0];
-
       });
     },
     createGame: function() {
@@ -148,7 +122,7 @@ new Vue({
       console.log("Initiating transaction... (please wait)");
 
       var meta;
-      MultiRPS.deployed().then(function(instance) {
+      return MultiRPS.deployed().then(function(instance) {
         console.log('first then');
         meta = instance;
         return meta.createGame({from: vm.account, value: web3.toWei(2, 'ether')});
@@ -164,36 +138,34 @@ new Vue({
             break;
           }
         }
-
+        vm.refresh();
       }).catch(function(e) {
         console.log(e);
         console.log("Error sending coin; see log.");
       });
     },
     joinGame: function() {
-      var vm = this;
       console.log("Initiating transaction... (please wait)");
-
+      var vm = this;
       var meta;
-      MultiRPS.deployed().then(function(instance) {
+      return MultiRPS.deployed().then(function(instance) {
         console.log('first then');
         meta = instance;
         return meta.joinGame(vm.joinGameId, {from: vm.account, value: web3.toWei(2, 'ether')});
       }).then(function(result) {
         console.log("Transaction complete!", result);
-
         // Catch GameCreated Event
         for (var i = 0; i < result.logs.length; i++) {
           var log = result.logs[i];
-
           if (log.event == "GameJoined") {
             vm.gameId = log.args.gameId.valueOf();
             break;
           }
         }
-
+        vm.refresh();
       }).catch(function(e) {
         console.log(e);
+
         console.log("Error sending coin; see log.");
       });
     }
@@ -202,7 +174,6 @@ new Vue({
     MultiRPS.setProvider(web3.currentProvider);
     console.log('provider set');
     this.getAccount();
-    this.getMygames();
-    this.getAvailableGames();
+    this.refresh();
   }
 })
